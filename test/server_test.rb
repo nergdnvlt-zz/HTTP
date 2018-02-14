6f3cab70d793748c592b433209660353c971d03e
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-13 20:52:22 -0700

    Working on faraday testing.

--- test/server_test.rb
+++ test/server_test.rb
@@ -1,14 +1,22 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
+require 'faraday'
+require './test/test_helper'
 
 require './lib/server'
 
-class ServerTest < MiniTest::Test
-  def test_count_method
-    server = Server.new
-    expected = server.count_response
 
-    assert_equal 1, expected
+class ServerTest < MiniTest::Test
+  def test_default_path
+    request = Faraday.get('http://127.0.0.1:9292/')
+    assert request.body.include?('Verb')
+  end
+
+  def test_hello_world_path
+    request = Faraday.get('http://127.0.0.1:9292/hello')
+    assert request.body.include?('Hello')
+  end
+
+  def test_date_path
+    request = Faraday.get('http://127.0.0.1:9292/datetime')
+    assert request.body.include?('2018')
   end
 end
