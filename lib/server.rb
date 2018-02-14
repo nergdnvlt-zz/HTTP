5181c230aeda3e0f236b8aa8150b6b6eb808e75d
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-14 08:53:30 -0700

    Refactoring after iteration 2.

--- lib/server.rb
+++ lib/server.rb
@@ -4,21 +4,23 @@ require './lib/path'
 # Creates a server object
 class Server
   include Path
+  attr_accessor :tcp_server
+
   def initialize
     @tcp_server = TCPServer.new(9292)
     @tcp_server.listen(1)
     @client = nil
     @request_lines = []
     @server_loop = true
+    @parser_output = ''
   end
 
   def start
     while @server_loop
       @client = @tcp_server.accept
       while_loop
-      puts @request_lines
-      response
-      @client.close
+      parser
+      printing
       @request_lines = []
     end
   end
@@ -30,19 +32,23 @@ class Server
   end
 
   def parser
-    Path.route(@request_lines)
+    @parser_output = Path.route(@request_lines)
   end
 
-  def response
-    response = '<pre>'"#{parser}"'</pre>'
-    output = "<html><head></head><body>#{response}</body></html>"
-    @server_loop = false if output.include?'Total'
-    headers = ['http/1.1 200 ok',
-               "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-               'server: ruby',
-               'content-type: text/html; charset=iso-8859-1',
-               "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  def headers
+    output = @parser_output
+    ['http/1.1 200 ok',
+     "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+     'server: ruby',
+     'content-type: text/html; charset=iso-8859-1',
+     "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
+
+  def printing
+    puts @request_lines
     @client.puts headers
-    @client.puts output
+    @client.puts @parser_output
+    @server_loop = false if @parser_output.include?'Total'
+    @client.close
   end
 end
